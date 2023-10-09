13f9ed23b65fbb2f0ad66057b28ca4ecf9bd132f
aliL2000 <aliladha2000@gmail.com>
2023-10-09 18:26:16 -0400

    Adding django migration and initial test

--- backend/application/tests.py
+++ backend/application/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from application.models import User, Contractor
 
-# Create your tests here.
+#TESTING USER CLASS
+class UserTestCases(TestCase):
+    def test_create_user_with_all_fields(self):
+        # Create a user with all fields supplied
+        user = User.objects.create(
+            name="John Doe",
+            email="johndoe@example.com",
+            phoneNumber="1234567890",
+            address="123 Main St"
+        )
+
+        # Query the database to check if the user was created
+        user_exists = User.objects.filter(name="John Doe").exists()
+
+        # Assert that the user was created successfully
+        self.assertTrue(user_exists)
+
