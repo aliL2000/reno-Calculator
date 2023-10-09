e011ee1d573d739affe16109fa34c3e5f623ac46
aliL2000 <aliladha2000@gmail.com>
2023-10-09 17:58:51 -0400

    Adding User model to the db

--- backend/application/models.py
+++ backend/application/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=15)
+    location = models.CharField(max_length=100)
+
+    #User must have EITHER email OR phoneNumber provided
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_either_email_or_phoneNumber",
+                check=(
+                    models.Q(email__isnull=False) | models.Q(phoneNumber__isnull=False)
+                ),
+            )
+        ]
