067bf8a609aa0f31a5c73592d6e7be86603a2afb
aliL2000 <aliladha2000@gmail.com>
2023-10-17 12:21:51 -0400

    First commit of djangorest

---
+++ backend/renoCalc/serializers.py
@@ -0,0 +1,8 @@
+from django.core import serializers
+from .models import Contractor
+from rest_framework import serializers
+
+class ContractorModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contractor
+        fields = ("name","email","phoneNumber","address","website","rating")
