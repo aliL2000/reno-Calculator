74ef59193ae12c6c3a77651af7f807ec5f675f8a
aliL2000 <aliladha2000@gmail.com>
2023-10-18 14:17:20 -0400

    Adding tests

---
+++ backend/renoCalc/tests/testArchitect.py
@@ -0,0 +1,37 @@
+from django.db import IntegrityError
+from django.test import TestCase
+from renoCalc.models import Contractor, Architect
+from django.core.management import call_command
+
+
+class ArchitectTestCases(TestCase):
+    def setUp(self):
+        call_command("flush", interactive=False)
+        self.contractorWithAllFields = Contractor.objects.create(
+            name="John Doe",
+            email="johndoe@example.com",
+            phoneNumber="1234567890",
+            address="123 Main St",
+            website_link="https://test.com",
+        )
+        self.contractorWithAllFields.full_clean()
+        self.contractorWithAllFields.save()
+        self.assertTrue(Contractor.objects.filter(name="John Doe").exists())
+
+    def testArchitectWithContractorMade(self):
+        commission_data = {"Home": 1_000_000, "Detached": 500_000, "Semi": 200_000}
+        ArchitectServiceWithAllFields = Architect.objects.create(
+            contractor=self.contractorWithAllFields,
+            description="testing",
+            commission=commission_data,
+        )
+
+        self.assertTrue(Architect.objects.filter(description="testing").exists())
+
+    def testArchitectWithoutNecessaryField(self):
+        with self.assertRaises(IntegrityError):
+            ArchitectServiceWithSomeFields = Architect.objects.create(
+                contractor=self.contractorWithAllFields,
+                description="test",
+            )
+            self.assertTrue(Architect.objects.filter(description="test").exists())
