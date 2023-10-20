7de4585be52d31001c5936158d606ef288a5d10b
aliL2000 <aliladha2000@gmail.com>
2023-10-19 22:41:20 -0400

    Adding url routing and view processing

--- backend/renoCalc/views.py
+++ backend/renoCalc/views.py
@@ -1,6 +1,19 @@
 from .models import Contractor
 from .serializers import ContractorModelSerializer
 from rest_framework import viewsets
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+
+@require_POST
+def my_view(request):
+    # Your view logic here
+    return HttpResponse("This is a POST request.")
+
+# @require_http_methods(["GET", "POST"])
+# def my_view(request):
+#     # I can assume now that only GET or POST requests make it this far
+#     # ...
+#     pass
 
 
 class ContractorViewSet(viewsets.ModelViewSet):
