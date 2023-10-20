82d9f59f3e7acc4ad60e25a7cc31d6352dd3bf44
aliL2000 <aliladha2000@gmail.com>
2023-10-19 22:58:11 -0400

    removing unused import

--- backend/renoCalc/models.py
+++ backend/renoCalc/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
-from django.forms import JSONField
 
 
 class UserModel(models.Model):
