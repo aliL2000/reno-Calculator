32927144adf2bd0cf1fecac3a253193c13b87ce0
aliL2000 <aliladha2000@gmail.com>
2023-10-24 16:09:26 -0400

    modifying explanation page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -75,14 +75,6 @@ export default function Home() {
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
       </footer>
 
       <style jsx>{`
--- frontend/pages/learn/explained.tsx
+++ frontend/pages/learn/explained.tsx
@@ -1,8 +1,13 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>Project Estimation Explained</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
