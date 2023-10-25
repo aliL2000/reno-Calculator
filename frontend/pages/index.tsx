6e429526d993a2c5223051eaf678a05e1c0bb4cd
aliL2000 <aliladha2000@gmail.com>
2023-10-25 12:53:16 -0400

    Adding global component layouts

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -75,6 +75,7 @@ export default function Home() {
       </main>
 
       <footer>
+        asd
       </footer>
 
       <style jsx>{`
@@ -86,24 +87,7 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
+
         code {
           background: #fafafa;
           border-radius: 5px;
