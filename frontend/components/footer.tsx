ad5986e134547c5efcdd2207f178842ec2766b9a
aliL2000 <aliladha2000@gmail.com>
2023-10-26 01:17:25 -0400

    Adding baseline footer styling

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
-import styles from './layout.module.css';
+import React from "react";
+import styles from "./layout.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Your Website Name</p>
-      </div>
+      <section className={styles.footerSignUp}>
+        <div>
+          <h3>ARE YOU A HOMEOWNER?</h3>
+        </div>
+        <div>
+          <h3>ARE YOU A CONTRACTOR?</h3>
+        </div>
+      </section>
+      <section className={styles.footerInfo}>
+        <div>
+          <h3>Homeowner</h3>
+          <Link href={'./'}>
+            Renovation Cost Calculator
+          </Link>
+        </div>
+        <div>
+          <h3>Contractor</h3>
+          <Link href={'./'}>
+            Register your business
+          </Link>
+          <Link href={'./'}>
+            Guidelines
+          </Link>
+        </div>
+        <div>
+          <h3>Learn</h3>
+          <Link href={'./'}>
+            Project Costs Explained
+          </Link>
+          <Link href={'./'}>
+            FAQ's
+          </Link>
+        </div>
+        <div>
+          <h3>About Us</h3>
+          <Link href={'./'}>
+            How it works
+          </Link>
+          <Link href={'./'}>
+            Who we are
+          </Link>
+          <Link href={'./'}>
+            Contact Us
+          </Link>
+          <Link href={'./'}>
+            Happiness Gaurantee
+          </Link>
+        </div>
+      </section>
+      
     </footer>
   );
 };
